e1d968cd72908e786c49dcace69037764125ee40
Favien Remond <f.remond@unistra.fr>
2024-11-28 17:35:14 +0100

    insertion des challenges et creation de la fonction afficheChallenges

--- P11/yams.cs
+++ P11/yams.cs
@@ -2,30 +2,63 @@ using System;
 using System.IO;
 
 class Yams{
-    public struct Challenge{
+    public struct Challenge
+    {
         public string nom;
         public string nomAfficher;
         public string objectif;
         public string nbPoint;
+        public Challenge(string n, string nA, string o, string nbP)
+        {
+            nom = n;
+            nomAfficher = nA;
+            objectif = o;
+            nbPoint = nbP;
+        }
     }
-    public struct Joueur{
+    public struct Joueur
+    {
         public int id;
         public string pseudo;
         public int[] scoreParTour;
         public string[] challengeParTour;
+        public List<Challenge> challengeDispo;
         //public int scoreTotal;
-        public Joueur(int i, string pse){
+        public Joueur(int i, string pse)
+        {
             id=i;
             pseudo=pse;
             scoreParTour = new int[13];
             challengeParTour = new string[13];
+            challengeDispo = new List<Challenge>
+                                                {
+                                                    // Challenges mineurs
+                                                    new Challenge("nombre1", "Nombre de 1", "Obtenir le maximum de 1", "Somme des dés ayant obtenu 1"),
+                                                    new Challenge("nombre2", "Nombre de 2", "Obtenir le maximum de 2", "Somme des dés ayant obtenu 2"),
+                                                    new Challenge("nombre3", "Nombre de 3", "Obtenir le maximum de 3", "Somme des dés ayant obtenu 3"),
+                                                    new Challenge("nombre4", "Nombre de 4", "Obtenir le maximum de 4", "Somme des dés ayant obtenu 4"),
+                                                    new Challenge("nombre5", "Nombre de 5", "Obtenir le maximum de 5", "Somme des dés ayant obtenu 5"),
+                                                    new Challenge("nombre6", "Nombre de 6", "Obtenir le maximum de 6", "Somme des dés ayant obtenu 6"),
+                                                    new Challenge("bonus", "Bonus", "Si la somme de la partie mineure atteint 63", "35 points"),
+
+                                                    // Challenges majeurs
+                                                    new Challenge("brelan", "Brelan", "Obtenir 3 dés de même valeur", "Somme des 3 dés identiques"),
+                                                    new Challenge("carre", "Carré", "Obtenir 4 dés de même valeur", "Somme des 4 dés identiques"),
+                                                    new Challenge("full", "Full", "Obtenir 3 dés de même valeur + 2 dés de même valeur", "25 points"),
+                                                    new Challenge("petite", "Petite suite", "Obtenir 1-2-3-4 ou 2-3-4-5 ou 3-4-5-6", "30 points"),
+                                                    new Challenge("grande", "Grande suite", "Obtenir 1-2-3-4-5 ou 2-3-4-5-6", "40 points"),
+                                                    new Challenge("yams", "Yam's", "Obtenir 5 dés de même valeur", "50 points"),
+                                                    new Challenge("chance", "Chance", "Obtenir le maximum de points", "Le total des dés obtenus")
+                                                };
         }
     }
-    public struct Partie{
+    public struct Partie
+    {
         public Joueur[] joueur;
         public int tours;
         public string date;
-        public Partie(Joueur j1, Joueur j2, DateTime d){
+        public Partie(Joueur j1, Joueur j2, DateTime d)
+        {
             joueur = new Joueur[2] {j1,j2};
             tours=0;
             date=d.toString("yyyy-MM-dd");
@@ -72,13 +105,14 @@ class Yams{
             i++;
         }
     }
-    public static void choixDes(int[] des, bool[] relancerDes){
+    public static void choixDes(int[] des, bool[] relancerDes)
+    {
         Console.WriteLine("Quelle des voulez vous gardez ?");
         afficheDes(des);
         int index=0;
         while(index<6){
             index=int.Parse(Console.ReadLine())
-            relancerDes[index]=false;
+            relancerDes[index-1]=false;
         }
     }
     public static void afficheDes(int[] des)
@@ -88,6 +122,13 @@ class Yams{
             Console.Write("{0}. {1} ; ",i+1,des[i]);
         }
         Console.WriteLine("{0}. Aucun",des.Length+1);
+    }
+    public static void afficheChallenges(Joueur j)
+    {
+        for(int i=0; i<j.challengeDispo.Count; i++)
+        {
+            Console.WriteLine("{0}. {1} ; {2} ; {3}", i+1, j.challengeDispo[i].nomAfficher, j.challengeDispo[i].objectif, j.challengeDispo[i].nbPoint);
+        }
     }
     public static Partie initialisationJeu()
     {
