8652e0c85bbea21fc4aa8d701076872923e87130
Favien Remond <f.remond@unistra.fr>
2024-11-28 23:54:04 +0100

    fin du jeu, reste a corriger les bug et créer le json *drop the mic*

--- P11/yams.cs
+++ P11/yams.cs
@@ -1,9 +1,13 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
 
 class Yams{
     public struct Challenge
     {
+        //public int code;
         public string nom;
         public string nomAfficher;
         public string objectif;
@@ -22,14 +26,15 @@ class Yams{
         public string pseudo;
         public int[] scoreParTour;
         public string[] challengeParTour;
+        public int totalScoreChallengeMineure;
         public List<Challenge> challengeDispo;
-        //public int scoreTotal;
         public Joueur(int i, string pse)
         {
             id=i;
             pseudo=pse;
             scoreParTour = new int[13];
             challengeParTour = new string[13];
+            totalScoreChallengeMineure = 0;
             challengeDispo = new List<Challenge>
                                                 {
                                                     // Challenges mineurs
@@ -39,7 +44,6 @@ class Yams{
                                                     new Challenge("nombre4", "Nombre de 4", "Obtenir le maximum de 4", "Somme des dés ayant obtenu 4"),
                                                     new Challenge("nombre5", "Nombre de 5", "Obtenir le maximum de 5", "Somme des dés ayant obtenu 5"),
                                                     new Challenge("nombre6", "Nombre de 6", "Obtenir le maximum de 6", "Somme des dés ayant obtenu 6"),
-                                                    new Challenge("bonus", "Bonus", "Si la somme de la partie mineure atteint 63", "35 points"),
 
                                                     // Challenges majeurs
                                                     new Challenge("brelan", "Brelan", "Obtenir 3 dés de même valeur", "Somme des 3 dés identiques"),
@@ -60,8 +64,8 @@ class Yams{
         public Partie(Joueur j1, Joueur j2, DateTime d)
         {
             joueur = new Joueur[2] {j1,j2};
-            tours=0;
-            date=d.toString("yyyy-MM-dd");
+            tours=1;
+            date=d.ToString("yyyy-MM-dd");
         }
     }
     static void Main()
@@ -69,9 +73,9 @@ class Yams{
         bool encoreJouer=true;
         while(encoreJouer)
         {
-            jeu()
+            jeu();
             Console.WriteLine("Voulez vous relancer une partie ? o/n");
-            if(Console.ReadLine=="n")
+            if(Console.ReadLine()=="n")
             {
                 encoreJouer=false;
             }
@@ -81,11 +85,12 @@ class Yams{
         Partie game = initialisationJeu();
         for(int i=0; i<13; i++)
         {
-            tour(game.joueur[0]);
-            tour(game.joueur[1]);
+            tour(ref game.joueur[0],game.tours-1);
+            tour(ref game.joueur[1],game.tours-1);
+            game.tours++;
         }
     }
-    public static void tour(Joueur j){
+    public static void tour(ref Joueur joueur, int tours){
         Random rnd = new Random();
         int[] des = new int[5];
         bool[] relancerDes = new bool[5] {true,true,true,true,true};
@@ -99,11 +104,15 @@ class Yams{
                 if(relancerDes[j]==true)
                 {
                     des[j]=rnd.Next(1,6);
+                    Thread.Sleep(10);
                 }
             }
             choixDes(des,relancerDes);
             i++;
         }
+        int index = choixChallenge(joueur);
+        joueur.scoreParTour[tours]=calculScore(ref joueur,des,index-1);
+        joueur.challengeDispo.RemoveAt(index-1);
     }
     public static void choixDes(int[] des, bool[] relancerDes)
     {
@@ -111,9 +120,190 @@ class Yams{
         afficheDes(des);
         int index=0;
         while(index<6){
-            index=int.Parse(Console.ReadLine())
+            index=int.Parse(Console.ReadLine());
             relancerDes[index-1]=false;
+            Console.WriteLine("le dès numero {0} ne sera pas lancé lors du prochain lancé",index);
         }
+    }
+    public static int calculScore(ref Joueur j, int[] des, int indexChallenge)
+    {
+        int nbPoint=0;
+        switch(j.challengeDispo[indexChallenge].nom)
+        {
+            case "nombre1":
+            nbPoint = 1*nbOcc(des,1);
+            j.totalScoreChallengeMineure+=nbPoint;
+            break;
+            case "nombre2":
+            nbPoint = 2*nbOcc(des,2);
+            j.totalScoreChallengeMineure+=nbPoint;
+            break;
+            case "nombre3":
+            nbPoint = 3*nbOcc(des,3);
+            j.totalScoreChallengeMineure+=nbPoint;
+            break;
+            case "nombre4":
+            nbPoint = 4*nbOcc(des,4);
+            j.totalScoreChallengeMineure+=nbPoint;
+            break;
+            case "nombre5":
+            nbPoint = 5*nbOcc(des,5);
+            j.totalScoreChallengeMineure+=nbPoint;
+            break;
+            case "nombre6":
+            nbPoint = 6*nbOcc(des,6);
+            j.totalScoreChallengeMineure+=nbPoint;
+            break;
+            case "brelan":
+            nbPoint = verifBrelan(des);
+            break;
+            case "carre":
+            nbPoint = verifCarre(des);
+            break;
+            case "full":
+            nbPoint = verifFull(des);
+            break;
+            case "petite":
+            if(monotonie(des)>=4)
+            {
+                nbPoint = 30;
+            }
+            break;
+            case "grande":
+            if(monotonie(des)>=5)
+            {
+                nbPoint = 40;
+            }
+            break;
+            case "yams":
+            if(nbOcc(des,des[0])==5)
+            {
+                nbPoint=50;
+            }
+            break;
+            case "chance":
+            nbPoint = somme(des);
+            break;
+            default:
+            Console.WriteLine("ya une erreur la mon reuf");
+            break;
+        }
+        return nbPoint;
+    }
+    public static int nbOcc(int[] t, int nb)
+    {
+        int cpt = 0;
+        for(int i=0; i<t.Length;i++)
+        {
+            if (t[i]==nb)
+            {
+                cpt++;
+            }
+        }
+        return cpt;
+    }   
+    public static int somme(int[] tab)
+    {
+        int somme=0;
+        for(int i=0; i<tab.Length; i++)
+        {
+            somme+=tab[i];
+        }
+        return somme;
+    }
+    public static int monotonie(int[] listeInitial)
+    {
+        int cpt=0;
+        int tailleMax=0;
+        for(int i=0;i<listeInitial.Length-1;i++){
+            if(listeInitial[i]<listeInitial[i+1]){
+                cpt++;
+            }
+            else{
+                if(cpt>tailleMax){
+                    tailleMax=cpt;
+                    cpt=0;
+                }
+            }
+        }
+        return tailleMax;
+    }
+    public static int verifBrelan(int[] des)
+    {
+        List<int> nbATester = listeNombreDifferent(des);
+        if(nbATester.Count>3)
+        {
+            return 0;
+        }
+        else
+        {
+            int nbOccurence;
+            for(int i=0; i<nbATester.Count; i++)
+            {
+                nbOccurence = nbOcc(des,nbATester[i]);
+                if(nbOccurence >=3)
+                {
+                    return nbATester[i]*3;
+                }
+            }
+        }
+        return 0;
+    }
+    public static int verifCarre(int[] des)
+    {
+        List<int> nbATester = listeNombreDifferent(des);
+        if(nbATester.Count>2)
+        {
+            return 0;
+        }
+        else
+        {
+            int nbOccurence;
+            for(int i=0; i<nbATester.Count; i++)
+            {
+                nbOccurence =nbOcc(des,nbATester[i]);
+                if(nbOccurence >=4)
+                {
+                    return nbATester[i]*4;
+                }
+            }
+        }
+        return 0;
+    }
+    public static int verifFull(int[] des)
+    {
+        List<int> nbATester = listeNombreDifferent(des);
+        if(nbATester.Count>2)
+        {
+            return 0;
+        }
+        else
+        {
+            int nbOccurence =nbOcc(des,nbATester[0]);
+            if(nbOccurence ==3 || nbOccurence ==2)
+            {
+                return 25;
+            }
+        }
+        return 0;
+    }
+    public static List<int> listeNombreDifferent(int[] des)
+    {
+        List<int> listeNombre = new List<int>();
+        for(int i=1; i<=6; i++)
+        {
+            if(des.Contains(i))
+            {
+                listeNombre.Add(i);
+            }
+        }
+        return listeNombre;
+    }
+    public static int choixChallenge(Joueur j)
+    {
+        afficheChallenges(j);
+        Console.WriteLine("Qu'elle chalenge voulez vous choisir ?");
+        return int.Parse(Console.ReadLine());
     }
     public static void afficheDes(int[] des)
     {
